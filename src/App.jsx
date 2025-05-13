4c255b277786fe44ae7f314df2c10346ba00e39c
Maheshika01 <maheshikachamodi01@gmail.com>
2025-05-14 01:04:52 +0530

    add roadmap section and some images

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Services from "./components/Services";
+import Roadmap from "./components/Roadmap";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
        
       </div>
 
--- src/components/design/Services.jsx
+++ src/components/design/Services.jsx
@@ -33,7 +33,7 @@ export const VideoChatMessage = () => {
           src={talkgenieWhiteSymbol}
           width={26}
           height={26}
-          alt="Brainwave"
+          alt="TalkGenie"
         />
       </div>
       <p className="tagline absolute right-2.5 bottom-1 text-[0.625rem] text-n-3 uppercase">
