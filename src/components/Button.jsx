f05283b6d19db2e8d959b48147ddb55c3579557f
Maheshika01 <maheshikachamodi01@gmail.com>
2025-05-15 00:39:59 +0530

    add some changes to buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,26 +1,34 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, href, onClick, children, px, white }) => {
-  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 rounded-full ${
     px || "px-7"
-  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
-
+  } ${white ? "text-n-8 bg-white" : "text-n-1"} ${className || ""}`;
   const spanClasses = "relative z-10";
 
   const renderButton = () => (
-    <button className={classes} onClick={onClick}>
-      <span>{children}</span>
+    <button 
+      className={classes} 
+      onClick={onClick}
+      style={white ? { border: 'none' } : {}}
+    >
+      <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
+
   const renderLink = () => (
-    <a href={href} className={classes}>
+    <a 
+      href={href} 
+      className={classes}
+      style={white ? { border: 'none' } : {}}
+    >
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </a>
   );
+
   return href ? renderLink() : renderButton();
-  return renderButton();
 };
 
 export default Button;
